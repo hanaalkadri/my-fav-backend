9daa035a9c9c82d60363c57bfc474d61e867c074
hanaalkadri <hanaalkadry@gmail.com>
2025-05-30 19:57:57 +0200

    Add delete and get all products API

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 4000;
 const multer = require("multer");
 const path = require("path");
+const { type } = require("os");
 
 
 
@@ -42,6 +43,87 @@ app.post("/upload",upload.single('product'),(req,res)=>{
     success: 1,
     image_url:`http://localhost:${port}/images/${req.file.filename}`
   })
+})
+
+//Schema for Creating Products
+
+const Product = mongoose.model("Product",{
+  id:{
+    type: Number,
+    required:true,
+  },
+  name:{
+    type:String,
+    required:true,
+  },
+  image:{
+    type:String,
+    required:true,
+  },
+  category:{
+    type:String,
+    required:true,
+  },
+  new_price:{
+    type:Number,
+    required:true,
+  },
+  old_price:{
+    type:Number,
+    required:true,
+  },
+  date:{
+    type:Date,
+    default:Date.now,
+  },
+  availabe:{
+    type:Boolean,
+    default:true,
+  },
+
+})
+
+app.post('/addproduct',async(req,res)=>{
+    let products = await Product.find({});
+let id = 1;
+if (products.length > 0) {
+  const last_product = products[products.length - 1];
+  id = typeof last_product.id === 'number' ? last_product.id + 1 : 1;
+}else{
+      id=1;
+    }
+    const product = new Product({
+      id:id,
+      name:req.body.name,
+      image:req.body.image,
+      category:req.body.category,
+      new_price:req.body.new_price,
+      old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+      success:true,
+      name:req.body.name,
+    })
+})
+
+//Creating API for Deleting Products
+app.post('/removeproduct', async (req, res) => {
+  await Product.findOneAndDelete({ id: req.body.id });
+  console.log("Removed");
+  res.json({
+    success: true,
+    name: req.body.name
+  });
+});
+
+//Creating API for getting all products
+app.get('/allproducts',async (req,res)=>{
+    let products = await Product.find({});
+    console.log("All Products Saved");
+    res.send(products);
 })
 
 app.listen(port,(error)=>{
